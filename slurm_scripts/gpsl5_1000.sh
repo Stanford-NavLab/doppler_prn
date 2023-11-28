9929255fe463c45ea4bc33584e3860d3f17803fe
Alan Yang <ayang1097@gmail.com>
2023-11-27 17:09:13 -0800

    update gps L5 doppler frequency

--- slurm_scripts/gpsl5_1000.sh
+++ slurm_scripts/gpsl5_1000.sh
@@ -26,13 +26,13 @@ lscpu
 
 mkdir results
 
-python3 run.py --s 0 --f 6e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
-python3 run.py --s 1 --f 6e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
-python3 run.py --s 2 --f 6e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
-python3 run.py --s 3 --f 6e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
-python3 run.py --s 4 --f 6e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
-python3 run.py --s 5 --f 6e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
-python3 run.py --s 6 --f 6e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
-python3 run.py --s 7 --f 6e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
-python3 run.py --s 8 --f 6e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
-python3 run.py --s 9 --f 6e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
+python3 run.py --s 0 --f 4.5e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
+python3 run.py --s 1 --f 4.5e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
+python3 run.py --s 2 --f 4.5e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
+python3 run.py --s 3 --f 4.5e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
+python3 run.py --s 4 --f 4.5e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
+python3 run.py --s 5 --f 4.5e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
+python3 run.py --s 6 --f 4.5e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
+python3 run.py --s 7 --f 4.5e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
+python3 run.py --s 8 --f 4.5e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
+python3 run.py --s 9 --f 4.5e3 --t 9.77517107e-8 --m 31 --n 10230 --gs 1000 --maxit 10_000_000 --name "results/gpsl5_1000" --log 10_000 --obj
