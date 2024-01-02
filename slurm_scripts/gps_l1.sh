1a217e5b82d3bb962d1894a64f005d898dd6c511
Alan Yang <ayang1097@gmail.com>
2024-01-02 22:36:25 +0800

    adjust script

--- slurm_scripts/gps_l1.sh
+++ slurm_scripts/gps_l1.sh
@@ -13,7 +13,7 @@
 #SBATCH --partition=normal
 #####################################
 
-# run using sbatch --array=0-9 gps_l1.sh to run with 10 random seeds
+# run using sbatch --array=0-100:10 gps_l1.sh to run with parameters 0,2,4,6,8
 
 # Load module for Gurobi and Julia (should be most up-to-date version, i.e. 1.7.2)
 module load python/3.9
@@ -29,4 +29,4 @@ lscpu
 mkdir results
 
 
-python3 run.py --s 0 --f 6e3 --t 9.77517107e-7 --m 31 --n 1023 --doppreg $SLURM_ARRAY_TASK_ID --maxit 10_000_000 --name "results/gps_l1" --log 10_000 --obj --obj_v_freq
+python3 run.py --s 0 --f 6e3 --t 9.77517107e-7 --m 31 --n 1023 --doppreg $SLURM_ARRAY_TASK_ID --maxit 10_000_000 --name "results/gps_l1" --log 1_000 --obj --obj_v_freq
